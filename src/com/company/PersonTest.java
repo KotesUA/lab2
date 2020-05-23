5c6591f6887c88516c55937530b9aa2f1473e8c8
Kotes <51745529+KotesUA@users.noreply.github.com>
2020-05-23 21:55:28 +0300

    added some comments

--- src/com/company/PersonTest.java
+++ src/com/company/PersonTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest {
     void testToFrom(){
-        //
+        //we use this to test the class with all non-final fields
         EqualsVerifier.forClass(Person.class).suppress(Warning.NONFINAL_FIELDS).verify();
     }
 }
